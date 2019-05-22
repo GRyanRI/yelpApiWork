c945f17bf4f685f8d18579bc62a512727a500056
GRyanRI <gregorywryan@gmail.com>
2019-05-22 17:41:02 -0400

    refactor

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,41 +1,27 @@
 import React from 'react';
 import './Business.css';
 
-const business = {
-    imageSrc: 'https://placeimg.com/300/300/any/grayscale',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-class Business extends React.Component {
+export class Business extends React.Component {
     render() {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src='https://placeimg.com/300/300/any/grayscale' />
+                    <img src={this.props.business.imageSrc} alt='' />
                 </div>
-                <h2>{ business.name }</h2>
+                <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{ business.address }</p>
-                        <p>{ business.city }</p>
-                        <p>{ business.state } { business.zipCode }</p>
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>ITALIAN</h3>
-                        <h3 className="rating">4.5 stars}</h3>
-                        <p>90 reviews</p>
+                        <h3>{this.props.business.category.toUpperCase()}</h3>
+                        <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+                        <p>{`${this.props.business.reviewCount} reviews`}</p>
                     </div>
                 </div>
             </div>
         );
     }
-} 
-
-export default Business;
+}
